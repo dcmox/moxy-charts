6b5c33cb35e37112903dab37e05134f0ad2609f9
dcmox <dancmox@comcast.net>
2020-02-17 20:43:51 -0600

    proxy

--- components/query.js
+++ components/query.js
@@ -1,4 +1,11 @@
 "use strict";
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 exports.__esModule = true;
 exports.query = function (selector) {
     if (typeof selector !== 'string') {
@@ -42,12 +49,72 @@ exports.bindAll = function (selector, fn) {
         fn(el);
     });
 };
+var queryMethods = {
+    remove: function (elements) {
+        elements.forEach(function (el) { return el.remove(); });
+    },
+    hide: function (elements) {
+        elements.forEach(function (el) { return (el.style.display = 'none'); });
+    },
+    show: function (elements) {
+        elements.forEach(function (el) {
+            el.style.display = 'block';
+            el.style.opacity = 1;
+        });
+    },
+    append: function (elements) {
+        var children = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            children[_i - 1] = arguments[_i];
+        }
+        elements.forEach(function (el) {
+            el.append.apply(el, children);
+        });
+    },
+    removeClass: function (elements, cls) {
+        elements.forEach(function (el) {
+            el.classList.remove(cls);
+        });
+    },
+    addClass: function (elements, cls) {
+        elements.forEach(function (el) {
+            el.classList.add(cls);
+        });
+    },
+    attr: function (elements, attr, value) {
+        elements.forEach(function (el) {
+            el.setAttribute(attr, value);
+        });
+    },
+    removeAttr: function (elements, attr) {
+        elements.forEach(function (el) {
+            el.removeAttribute(attr);
+        });
+    },
+    removeProp: function (elements, prop) {
+        elements.forEach(function (el) {
+            el.removeProperty(prop);
+        });
+    },
+    prop: function (elements, prop, value) {
+        elements.forEach(function (el) {
+            el.setProperty(prop, value);
+        });
+    }
+};
 var queryHandler = {
     get: function (target, keyOrMethod) {
         if (target.elements.length === 0) {
-            return null;
+            return function () {
+                var args = [];
+                for (var _i = 0; _i < arguments.length; _i++) {
+                    args[_i] = arguments[_i];
+                }
+                return null;
+            };
         }
-        else if (typeof target.elements[0][keyOrMethod] !== 'function') {
+        else if (typeof target.elements[0][keyOrMethod] !== 'function' &&
+            !queryMethods[keyOrMethod]) {
             return target.elements.map(function (el) { return el[keyOrMethod]; });
         }
         return function () {
@@ -56,7 +123,12 @@ var queryHandler = {
                 args[_i] = arguments[_i];
             }
             var result = target.elements.forEach(function (el) {
-                el[keyOrMethod](args);
+                if (el[keyOrMethod]) {
+                    el[keyOrMethod].apply(el, args);
+                }
+                else if (queryMethods[keyOrMethod]) {
+                    queryMethods[keyOrMethod].apply(queryMethods, __spreadArrays([el], args));
+                }
             });
             return result;
         };
--- components/query.ts
+++ components/query.ts
@@ -48,16 +48,73 @@ export const bindAll = (
 	})
 }
 
+const queryMethods = {
+	remove(elements: HTMLElement[]) {
+		elements.forEach((el: any) => el.remove())
+	},
+	hide(elements: HTMLElement[]) {
+		elements.forEach((el: any) => (el.style.display = 'none'))
+	},
+	show(elements: HTMLElement[]) {
+		elements.forEach((el: any) => {
+			el.style.display = 'block'
+			el.style.opacity = 1
+		})
+	},
+	append(elements: HTMLElement[], ...children: any) {
+		elements.forEach((el: any) => {
+			el.append(...children)
+		})
+	},
+	removeClass(elements: HTMLElement[], cls: string) {
+		elements.forEach((el: any) => {
+			el.classList.remove(cls)
+		})
+	},
+	addClass(elements: HTMLElement[], cls: string) {
+		elements.forEach((el: any) => {
+			el.classList.add(cls)
+		})
+	},
+	attr(elements: HTMLElement[], attr: string, value: string) {
+		elements.forEach((el: any) => {
+			el.setAttribute(attr, value)
+		})
+	},
+	removeAttr(elements: HTMLElement[], attr: string) {
+		elements.forEach((el: any) => {
+			el.removeAttribute(attr)
+		})
+	},
+	removeProp(elements: HTMLElement[], prop: string) {
+		elements.forEach((el: any) => {
+			el.removeProperty(prop)
+		})
+	},
+	prop(elements: HTMLElement[], prop: string, value: string) {
+		elements.forEach((el: any) => {
+			el.setProperty(prop, value)
+		})
+	},
+}
+
 const queryHandler = {
 	get: (target: any, keyOrMethod: string) => {
 		if (target.elements.length === 0) {
-			return null
-		} else if (typeof target.elements[0][keyOrMethod] !== 'function') {
+			return (...args: any) => null
+		} else if (
+			typeof target.elements[0][keyOrMethod] !== 'function' &&
+			!queryMethods[keyOrMethod]
+		) {
 			return target.elements.map((el: any) => el[keyOrMethod])
 		}
 		return (...args: any) => {
 			const result = target.elements.forEach((el: any) => {
-				el[keyOrMethod](args)
+				if (el[keyOrMethod]) {
+					el[keyOrMethod](...args)
+				} else if (queryMethods[keyOrMethod]) {
+					queryMethods[keyOrMethod](el, ...args)
+				}
 			})
 			return result
 		}
